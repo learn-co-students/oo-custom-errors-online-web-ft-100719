dbc1b7b14efa7c9f855c4d058a146875070d324f
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-23 20:36:51 +0000

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,12 +10,9 @@ class Person
     person.partner = self
   end
 
+class PartnerError < StandardError
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
